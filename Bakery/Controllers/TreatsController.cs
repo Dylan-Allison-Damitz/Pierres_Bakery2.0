22089869986480b5a1d905f823d33480c85a20d6
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-11-06 13:18:04 -0700

    add updates to treats controller authorization

--- Bakery/Controllers/TreatsController.cs
+++ Bakery/Controllers/TreatsController.cs
@@ -1,17 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Bakery.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Bakery.Controllers
 {
+
+    [Authorize]
     public class TreatsController : Controller
     {
         private readonly BakeryContext _db;
+        public readonly UserManager<ApplicationUser> _userManager;
 
-        public TreatsController(BakeryContext db)
+        public TreatsController(UserManager<ApplicationUser> userManager, BakeryContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
@@ -73,3 +82,4 @@ namespace Bakery.Controllers
         }
     }
 }
+
